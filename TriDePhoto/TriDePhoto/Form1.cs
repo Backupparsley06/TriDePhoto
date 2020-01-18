c9063e4c8f33edbc34855632f2434aeb330cca68
XxX69CockMaster69XxX <backupparsley06@gmail.com>
2020-01-18 13:26:41 -0500

    Photo loading

--- TriDePhoto/TriDePhoto/Form1.cs
+++ TriDePhoto/TriDePhoto/Form1.cs
@@ -18,12 +18,20 @@ namespace TriDePhoto
         {
             InitializeComponent();
             pictureLoader = new PictureLoader();
+            InitPictureBox();
 
+        }
+
+        private void InitPictureBox()
+        {
+            PB_affichageSac.Height = (int)((float)PB_affichageSac.Width / (8000.0 / 2048.0));
         }
 
         private void BTN_LoadPhoto_Click(object sender, EventArgs e)
         {
             pictureLoader.LoadAllPictureID();
+            Image image = pictureLoader.LoadImageById(0);
+            PB_affichageSac.Image = image;
         }
     }
 }
