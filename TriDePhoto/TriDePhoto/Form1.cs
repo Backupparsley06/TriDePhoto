9ca1f10ca0a307692d1f8e99f8edf9c4b8b1411f
XxX69CockMaster69XxX <backupparsley06@gmail.com>
2020-01-18 12:20:03 -0500

    yolo

---
+++ TriDePhoto/TriDePhoto/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriDePhoto
+{
+    public partial class TriDePhoto : Form
+    {
+        PictureLoader pictureLoader;
+        public TriDePhoto()
+        {
+            InitializeComponent();
+            pictureLoader = new PictureLoader();
+
+        }
+
+        private void BTN_LoadPhoto_Click(object sender, EventArgs e)
+        {
+            pictureLoader.LoadAllPictureID();
+        }
+    }
+}
---
+++ TriDePhoto/TriDePhoto/PictureLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriDePhoto
+{
+    class PictureLoader
+    {
+        public PictureLoader()
+        {
+
+        }
+
+        public void LoadAllPictureID()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            DirectoryInfo di = new DirectoryInfo("../../../..");
+        }
+    }
+}
