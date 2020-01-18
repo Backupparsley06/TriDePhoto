c923881ea27c6f5d1a4882a007426c3a7ed2c63b
XxX69CockMaster69XxX <backupparsley06@gmail.com>
2020-01-18 12:02:28 -0500

    Projet Initialise

---
+++ TriDePhoto/TriDePhoto/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriDePhoto
+{
+    public partial class TriDePhoto : Form
+    {
+        public TriDePhoto()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BTN_LoadPhoto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
